cf26508b420961e89bf7777ce04cf0c1c2d55f88
Felipe Finhane <felipefinhane@gmail.com>
2023-04-12 18:03:03 -0300

    feat: Including Telegram url

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module gin
 
 go 1.14
 
-require github.com/gin-gonic/gin v1.7.1 // indirect
+require (
+	github.com/gin-gonic/gin v1.7.1 // indirect
+	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible // indirect
+	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,15 +1,55 @@
 package main
 
 import (
+	"encoding/json"
+	"log"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
-var Router * gin.Engine
+
+var Router *gin.Engine
+
 func main() {
+
 	r := gin.Default()
+
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "Hello world!",
 		})
 	})
+
+	// Crie um novo bot com o token de acesso fornecido pelo BotFather
+	bot, err := tgbotapi.NewBotAPI("SEU_TOKEN_DE_ACESSO_AQUI")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	// Crie uma rota POST para o caminho /webhook
+	r.POST("/webhook", func(c *gin.Context) {
+		// Decodifique o JSON da solicitação para uma estrutura de atualização do bot do Telegram
+		var update tgbotapi.Update
+		if err := json.NewDecoder(c.Request.Body).Decode(&update); err != nil {
+			log.Printf("Error decoding request body: %s", err)
+			c.AbortWithStatus(http.StatusBadRequest)
+			return
+		}
+
+		// Verifique se a atualização recebida é uma mensagem
+		if update.Message == nil {
+			c.Status(http.StatusOK)
+			return
+		}
+
+		// Responda à mensagem do usuário com uma mensagem de saudação
+		reply := "Olá, " + update.Message.From.UserName + "! Eu sou um bot criado em Go!"
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, reply)
+		bot.Send(msg)
+
+		c.Status(http.StatusOK)
+	})
+
 	r.Run()
 }
